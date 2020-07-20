796f9f07914efbcbfd36795eb70ee6676aac4bb1
seba898 <48302014+seba898@users.noreply.github.com>
2020-07-20 14:39:06 -0400

    actual

---
+++ cunts/app/src/main/java/com/example/cunts/MainActivity.kt
@@ -0,0 +1,302 @@
+package com.example.cunts
+
+
+import android.Manifest
+import android.annotation.SuppressLint
+import android.content.pm.PackageManager
+import android.media.Image
+import android.net.Uri
+import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import androidx.camera.core.*
+import androidx.camera.lifecycle.ProcessCameraProvider
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import kotlinx.android.synthetic.main.activity_main.*
+import java.io.File
+import java.nio.ByteBuffer
+import java.text.SimpleDateFormat
+import java.util.*
+import java.util.concurrent.ExecutorService
+import java.util.concurrent.Executors
+
+
+typealias LumaListener = (luma: Double) -> Unit
+
+class MainActivity : AppCompatActivity() {
+    private var preview: Preview? = null
+    private var imageCapture: ImageCapture? = null
+    private var imageAnalyzer: ImageAnalysis? = null
+    private var camera: Camera? = null
+
+    private lateinit var outputDirectory: File
+    private lateinit var cameraExecutor: ExecutorService
+
+
+
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+
+
+
+        // Request camera permissions
+        if (allPermissionsGranted()) {
+            startCamera()
+        } else {
+            ActivityCompat.requestPermissions(
+                this, REQUIRED_PERMISSIONS, REQUEST_CODE_PERMISSIONS)
+        }
+
+        // Setup the listener for take photo button
+        camera_capture_button.setOnClickListener { takePhoto() }
+
+        outputDirectory = getOutputDirectory()
+
+        cameraExecutor = Executors.newSingleThreadExecutor()
+    }
+
+
+
+
+
+    private class LuminosityAnalyzer(private val listener: LumaListener) : ImageAnalysis.Analyzer {
+
+        private fun ByteBuffer.toByteArray(): ByteArray {
+            rewind()    // Rewind the buffer to zero
+            val data = ByteArray(remaining())
+            get(data)   // Copy the buffer into a byte array
+            return data // Return the byte array
+        }
+
+        @SuppressLint("UnsafeExperimentalUsageError")
+        override fun analyze(imageP: ImageProxy) {
+
+            val buffer = imageP.planes[0].buffer
+            val data = buffer.toByteArray()
+            val pixels = data.map { it.toInt() and 0xFF }
+            val luma = pixels.average()
+
+
+            val img = imageP.image//get actual image from proxy
+            val pb = KotlinSucks().toBitmap(img)//turn image into bitmap\
+//            Log.v("height", pb.height.toString())
+//            Log.v("width", pb.width.toString())
+//               Log.v("space", pb.colorSpace.toString())
+
+            val penis = KotlinSucks().kotlinSucksAss(pb)//get averages of RGB from bitmap
+
+//           Log.v("red top left", penis[0].toString())
+//            Log.v("green top left", penis[4].toString())
+//            Log.v("blue top left", penis[8].toString())
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        val path = "/storage/emulated/0/Android/media/com.example.cunts/cunts/2020-07-17-18-29-12-951.jpg"
+// /storage/emulated/0/Android/media/com.example.cunts/cunts/2020-07-17-18-29-12-951.jpg
+            val hello = KotlinSucks().uglies()
+
+
+
+
+
+
+
+
+
+            //           val iMG =  KotlinSucks().fuglies(hello)
+            //            Log.v("FUCK", iMG.height.toString())
+
+            val a = hello.byteCount
+
+
+
+            listener(luma)
+            imageP.close()
+        }
+    }
+
+
+
+
+
+
+
+    private fun startCamera() {
+        val cameraProviderFuture = ProcessCameraProvider.getInstance(this)
+
+        cameraProviderFuture.addListener(Runnable {
+            // Used to bind the lifecycle of cameras to the lifecycle owner
+            val cameraProvider: ProcessCameraProvider = cameraProviderFuture.get()
+
+            // Preview
+            preview = Preview.Builder()
+                .build()
+
+            imageCapture = ImageCapture.Builder()
+                .setCaptureMode(ImageCapture.CAPTURE_MODE_MINIMIZE_LATENCY)
+                .build()
+
+            imageAnalyzer = ImageAnalysis.Builder().setBackpressureStrategy(ImageAnalysis.STRATEGY_KEEP_ONLY_LATEST)
+                .build()
+                .also {
+                    it.setAnalyzer(cameraExecutor, LuminosityAnalyzer { luma ->
+                        Log.d(TAG, "Average luminosity: $luma")
+                    })
+
+                }
+            // Select back camera
+            val cameraSelector = CameraSelector.Builder().requireLensFacing(CameraSelector.LENS_FACING_BACK).build()
+
+            try {
+                // Unbind use cases before rebinding
+                cameraProvider.unbindAll()
+
+                // Bind use cases to camera
+                camera = cameraProvider.bindToLifecycle(
+                    this, cameraSelector, preview, imageCapture, imageAnalyzer)
+                preview?.setSurfaceProvider(viewFinder.createSurfaceProvider(camera?.cameraInfo))
+            } catch(exc: Exception) {
+                Log.e(TAG, "Use case binding failed", exc)
+            }
+
+        }, ContextCompat.getMainExecutor(this))
+
+
+    }
+
+    private fun takePhoto() {
+        // Get a stable reference of the modifiable image capture use case
+        val imageCapture = imageCapture ?: return
+
+        // Create timestamped output file to hold the image
+        val photoFile = File(
+            outputDirectory,
+            SimpleDateFormat(FILENAME_FORMAT, Locale.US
+            ).format(System.currentTimeMillis()) + ".jpg")
+
+        // Create output options object which contains file + metadata
+        val outputOptions = ImageCapture.OutputFileOptions.Builder(photoFile).build()
+
+        // Setup image capture listener which is triggered after photo has
+        // been taken
+        imageCapture.takePicture(
+            outputOptions, ContextCompat.getMainExecutor(this), object : ImageCapture.OnImageSavedCallback {
+                override fun onError(exc: ImageCaptureException) {
+                    Log.e(TAG, "Photo capture failed: ${exc.message}", exc)
+                }
+
+                override fun onImageSaved(output: ImageCapture.OutputFileResults) {
+                    val savedUri = Uri.fromFile(photoFile)
+                    val msg = "Photo capture succeeded: $savedUri"
+                    Toast.makeText(baseContext, msg, Toast.LENGTH_SHORT).show()
+                    Log.d(TAG, msg)
+                }
+            })
+    }
+
+    private fun allPermissionsGranted() = REQUIRED_PERMISSIONS.all {
+        ContextCompat.checkSelfPermission(
+            baseContext, it) == PackageManager.PERMISSION_GRANTED
+    }
+
+    fun getOutputDirectory(): File {
+        val mediaDir = externalMediaDirs.firstOrNull()?.let {
+            File(it, resources.getString(R.string.app_name)).apply { mkdirs() } }
+        return if (mediaDir != null && mediaDir.exists())
+            mediaDir else filesDir
+    }
+
+    companion object {
+        private const val TAG = "CameraXBasic"
+        private const val FILENAME_FORMAT = "yyyy-MM-dd-HH-mm-ss-SSS"
+        private const val REQUEST_CODE_PERMISSIONS = 10
+        private val REQUIRED_PERMISSIONS = arrayOf(Manifest.permission.CAMERA)
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int, permissions: Array<String>, grantResults:
+        IntArray) {
+        if (requestCode == REQUEST_CODE_PERMISSIONS) {
+            if (allPermissionsGranted()) {
+                startCamera()
+            } else {
+                Toast.makeText(this,
+                    "Permissions not granted by the user.",
+                    Toast.LENGTH_SHORT).show()
+                finish()
+            }
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+//region eat my balls
+//            val even: IntArray = intArrayOf(2, 4, 6)
+//            val odd: IntArray = intArrayOf(1, 3, 5)
+//
+//            val lala: Array<IntArray> = arrayOf(even, odd)
+//            val rtl = arrayOfNulls<Int>(76800)
+//            val gtl = arrayOfNulls<Int>(76800)
+//            val btl = arrayOfNulls<Int>(76800)
+//
+//            for(i in 0 until 240) {
+//                for(y in 0 until 320) {
+//                    val rtl[y] = 10
+//                    val gtl = null
+//                    val btl = null
+//                }
+//
+//
+//            val rtl = null
+//            val gtl = null
+//            val btl = null
+//
+//            val rtr = null
+//            val gtr = null
+//            val btr = null
+//
+//            val rbl = null
+//            val gbl = null
+//            val bbl = null
+//
+//            val rbr = null
+//            val gbr = null
+//            val bbr = null
+//
+//
+//
+//
+//            val arr = arrayOf(
+//                arrayOf(intArrayOf(0, 0), intArrayOf(0)),
+//                arrayOf(intArrayOf(0, 1), intArrayOf(0)),
+//                arrayOf(intArrayOf(1, 0), intArrayOf(0)),
+//                arrayOf(intArrayOf(1, 1), intArrayOf(1))
+//            )
+//endregion
+
